001494eeb4e6e2d96d5b600d7fde5d2b3cadf1ae
cold <1812821235@qq.com>
2019-11-18 14:42:35 +0800

    添加sourceSets用法

--- app/src/main/java/com/cold/gradle/MainActivity.java
+++ app/src/main/java/com/cold/gradle/MainActivity.java
@@ -5,20 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cold.gradle.interfaces.IFlavors;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView imgvTest;
     private TextView tvFlavorName;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgvTest = findViewById(R.id.imgv_test);
         tvFlavorName = findViewById(R.id.tv_flavor_name);
+        setImage();
         setFlavorsName();
+    }
+    
+    private void setImage() {
+        imgvTest.setImageResource(R.drawable.ic_test);
     }
 
     private void setFlavorsName() {
