895f2b6ab7baf13d17ed08a191cdc4a4c3698612
cold <1812821235@qq.com>
2019-11-21 11:44:56 +0800

    添加自定义插件扩展属性获取的方式

--- buildSrc/src/main/groovy/com/cold/plugin/MyPlugin.groovy
+++ buildSrc/src/main/groovy/com/cold/plugin/MyPlugin.groovy
@@ -8,8 +8,21 @@ class MyPlugin implements Plugin<Project> {
         System.out.println("========================")
         System.out.println("这是个插件!")
         System.out.println("========================")
+
+//        project.extensions.create('person', Person)
+        project.getExtensions().create('person', Person)
         
+        project.task('getExtension') {
+            doLast {
+                def person = project['person']
+                System.out.println("========================> person name: " + person.name + "  num: " + person.num + "  sex: " + person.sex)
+
+            }
+        }
+
     }
 
+    
+    
 }
 
---
+++ buildSrc/src/main/groovy/com/cold/plugin/Person.groovy
@@ -0,0 +1,9 @@
+package com.cold.plugin
+
+class Person {
+    
+    String name = null
+    String num = null
+    String sex = null
+
+}
